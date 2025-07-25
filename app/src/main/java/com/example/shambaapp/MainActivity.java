ffb3eb291c1cf68d9c0c6c5d461a4b43444b1f21
Robin Aluma <alumarobin@gmail.com>
2025-07-25 13:51:39 +0300

    First commit

---
+++ app/src/main/java/com/example/shambaapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.shambaapp;
+import android.view.View;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button register = findViewById(R.id.register);
+                register.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        Intent intent = new Intent(MainActivity.this,SignUp.class);
+                        startActivity(intent);
+                    }
+                });
+        Button login = findViewById(R.id.login);
+                login.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        Intent intent = new Intent(MainActivity.this,SignIn.class);
+                        startActivity(intent);
+                    }
+                });
+    }
+}
